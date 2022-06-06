aba8412d7b48b4611206e85f2c2532c1b2b4ef3e
Kevin <NateraKevin1@gmail.com>
2022-06-06 11:48:37 -0400

    created clock widget

--- skeleton/frontend/clock.jsx
+++ skeleton/frontend/clock.jsx
@@ -2,12 +2,37 @@ import React from 'react';
 
 class Clock extends React.Component {
     constructor(props) {
-        super(props);
+        super(props) 
+
+        this.state = {
+            time : new Date()
+        }
+
     }
 
 
     render() {
-        return (<h1> The Greatest Clock</h1>);
+        console.log(this.time)
+        return (<div> 
+            <h1>The Greatest Clock</h1>
+            <h2>  Time: {this.state.time.toTimeString()}  </h2>
+        </div>
+            
+        );
+    }
+
+    
+    componentDidMount() {
+        this.tickId = setInterval( () => { this.tick() } , 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.tickId)
+    }
+
+    tick() {
+        this.setState({time: new Date() })
+        console.log(this.state.time)   
     }
 }
 
